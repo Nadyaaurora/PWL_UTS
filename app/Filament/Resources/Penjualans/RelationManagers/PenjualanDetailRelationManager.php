b4dd4f91f0c823b14cdf44bfd870e9828f1c31b8
nadyaaurora <nadyaaurora575@gmail.com>
2026-04-17 09:53:09 +0700

    feat: add penjualan detail relation manager

---
+++ app/Filament/Resources/Penjualans/RelationManagers/PenjualanDetailRelationManager.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Filament\Resources\Penjualans\RelationManagers;
+
+use Filament\Actions\AssociateAction;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\CreateAction;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\DissociateAction;
+use Filament\Actions\DissociateBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Schemas\Schema;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class PenjualanDetailRelationManager extends RelationManager
+{
+    protected static string $relationship = 'penjualanDetail';
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('barang_id')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('barang_id')
+            ->columns([
+                TextColumn::make('barang_id')
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                CreateAction::make(),
+                AssociateAction::make(),
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DissociateAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DissociateBulkAction::make(),
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
